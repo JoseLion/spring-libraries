41d46d37e01395365a5c0cc63fc7fc57b4f7558e
lfgarcia <luis.garcia@provedatos.com>
2017-01-07 22:13:27 -0500

    - Ajuste para obtener los archivos.

--- JavaLibraries/src/main/java/ec/com/levelap/base/service/DocumentService.java
+++ JavaLibraries/src/main/java/ec/com/levelap/base/service/DocumentService.java
@@ -2,8 +2,10 @@ package ec.com.levelap.base.service;
 
 import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -22,31 +24,35 @@ public class DocumentService {
 
 	public String saveFile(MultipartFile file, String module) throws IOException {
 		if (environment.getProperty(fileEnvironment) == null) {
-			throw new IOException("NO SE PUDO ENCOTRAR LA VARIABLE DE ENTORNO " + fileEnvironment + " EN SU EQUIPO");
+			throw new IOException("NO SE PUDO ENCOTRAR LA VARIABLE DE ENTORNO ".concat(fileEnvironment).concat(" EN SU EQUIPO"));
 		}
-
 		StringBuilder path = new StringBuilder(environment.getProperty(fileEnvironment));
 		path.append(File.separator);
 		path.append(module);
-
 		File directory = new File(path.toString());
-		if(!directory.exists()) {
+		if (!directory.exists()) {
 			directory.mkdirs();
 		}
 		path.append(File.separator);
 		path.append(file.getOriginalFilename());
 		File serverFile = new File(path.toString());
-
 		byte[] bytes = file.getBytes();
 		BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(serverFile));
 		stream.write(bytes);
 		stream.close();
-
 		return path.toString();
 	}
-	
-	public MultipartFile getFile() {
-		return null;
+
+	public InputStream getFile(String module, String fileName) throws IOException {
+		if (environment.getProperty(fileEnvironment) == null) {
+			throw new IOException("NO SE PUDO ENCOTRAR LA VARIABLE DE ENTORNO ".concat(fileEnvironment).concat(" EN SU EQUIPO"));
+		}
+		StringBuilder path = new StringBuilder(environment.getProperty(fileEnvironment));
+		path.append(File.separator);
+		path.append(module);
+		path.append(File.separator);
+		path.append(fileName);
+		File serverFile = new File(path.toString());
+		return new FileInputStream(serverFile);
 	}
-	
 }
