65ec3eefc957d69c4ca789562f7948f23819141a
lfgarcia <luis.garcia@provedatos.com>
2017-05-29 14:33:25 -0500

    - Ajuste en crop de imágenes.

--- JavaLibraries/src/main/java/ec/com/levelap/blog/entity/BlogArticleOpen.java
+++ JavaLibraries/src/main/java/ec/com/levelap/blog/entity/BlogArticleOpen.java
@@ -3,24 +3,32 @@ package ec.com.levelap.blog.entity;
 import java.util.Date;
 import java.util.List;
 
+import org.postgresql.geometric.PGline;
+
 import ec.com.levelap.commons.archive.Archive;
 
 public interface BlogArticleOpen {
+
 	public Long getId();
-	
+
 	public String getTitle();
-	
+
 	public Date getCreationDate();
-	
+
 	public BlogExtra getCategory();
-	
+
 	public Archive getBanner();
-	
+
 	public String getAuthor();
-	
+
 	public List<BlogExtra> getTags();
-	
+
 	public void setTags(List<BlogExtra> tags);
-	
+
 	public String getSummary();
+
+	public PGline getDiamondCrop();
+
+	public PGline getSquareCrop();
+
 }
